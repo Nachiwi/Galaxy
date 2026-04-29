f560de4b8a7947f3f1698ae660ab9041d5c12b1a
Nachiwi <153971203+Nachiwi@users.noreply.github.com>
2026-04-28 20:18:05 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,182 @@
+const recuerdos = [
+  {
+    titulo: "Mi regalo de cumpleaños",
+    texto: "Despues de pasar uno de los dias más marcados de mi vida, fue el primer cumpleaños que celebre con mis amigos y especialmente contigo fue un mometo de muchas emociones que no habia experimentado antes, ese dia sin saber recibi uno de mis compañeros de sueño, a quien abrazo con la idea de que tambien sientes ese abrazo, quien me acompaña en las noches que me siento mal, que sufro o aquellas noches en las que paso frio, ese gesto tuyo que a echo mis noches y dias diferentes es un peluchin que guardare y cuidadare con mi vida.",
+    x: 20,
+    y: 30,
+    imagen: "assets/cumpleaños.jpg",
+    audio: ""
+  },
+  {
+    titulo: "Uno de tantos momentos juntos",
+    texto: "Aunque no tengamos muchas fotos juntos, las pocas que tenemos son muy especiales para mi, cada una de ellas me recuerda momentos muy felices a tu lado, como esta que es una de mis favoritas, donde puedes ver la confianza de ser yo que siento cuando estoy contigo, esa sonrisa tuya que hace que salga la mia y ese brillo en tus ojos que sale cuando los enchinas uffff enamoradisimo quedo.",
+    x: 60,
+    y: 50,
+    imagen: "assets/momento_iconic.jpg",
+    audio: ""
+  },
+ {
+    titulo: "Mi foto favorita",
+    texto: "En una de esas ocasiones muy especificas en las que nos tomamos fotos surge esta, una de mis fotos favoritas, donde se ve lo bonito que nos vemos, la felicidad que se puede notar por estar a tu lado, una muestra muy real de la tranquilidad y comodidad que siento al estar a tu lado, simplemente hermoso el compartir mi vida contigo.",
+    x: 30,
+    y: 40,
+    imagen: "assets/foto_fav.jpg",
+    audio: ""
+  },
+  {
+    titulo: "Woof Woof",
+    texto: "Jejeje que te puedo decir de esto mor, tu con una cadena y yo amarrado pero bien feliz, me gusta mucho el compartir noches de juego contigo, el sentimiento de conectarme a discord, buscar un juego para los  dos y despues olvidarme del juego pq estoy más disfrutando nuestra platica, el momento que nos tomamos para compartir algo juntos es algo muy lindo la verdad, muy disfurtable, muy entraable y con muchas risas de pormedio",
+    x: 70,
+    y: 20,
+    imagen: "assets/correa.jpeg",
+    audio: ""
+  },
+{
+    titulo: "La MARCA",
+    texto: "Aaaaay amor como olvidar aquel dia que se te altero la hormona y me diste tremenda mordida que hasta recuerdo dejo, como okvidar ese dia, ese dia andabas muy muy MUUUUY cariñosa lo cual me sorpendio en un momento y despues de eso yo solo me deje querer, pero me quisiste de más que dijiste, lo voy a marcar para que vean que ya es super mio y obiamiente siendo super tuyo me deje, aunque me hayas dicho que ya no abra una 2da marca yo confio en que si exita una 2da marca pq si se sintiio muy aca. muy aventurero muy experimental muy aventura de chavito bien",
+    x: 50,
+    y: 10,
+    imagen: "assets/marca.jpeg",
+    audio: ""
+  },
+  {
+    titulo: "Corazón",
+    texto: "Hay muchos momentos y muchos recuerdos los cuales no tienen fotos pero estan muy marcados en mi mente y corazón, tal vez este detalle no sea el mejor, no este lleno de estrellas como imginaba o como me gustaria que hubiese sido, pero amor mio, tu sabes que te lo hago con mucho cariño y dedicación, al ser una pagina que subi a plataforma me encargare de irla actualizando cada mes con los muchos o pocos momentos que compartimos y asi en un mucho o poco tiempo por fin tener una galaxia que cuente nuestra historia. ",
+    x: 70,
+    y: 60,
+    imagen: "assets/corazon.jpeg",
+    audio: ""
+  },
+   {
+    titulo: "El inicio",
+    texto: "Esta foto taaaaan iconica, foto que hasta el dia de hoy me hace que me quede embobado a la pantalla pensando en como es que llegamos tan lejos, sinceramente por la idea erronea que tenia de ti jamas me hubieras echo caso pero oh sopresa!!! no era asi y a dia de hoy me sigo preguntando como le hice o realmente que me viste pero fuaaaaaaa y es que tu no lo sabes pero tengo un theme en mi 3ds echo por mi donde esta un collague de fotos tuyas como fondo, osea mor, ya pasaste de mi fn a mi pc y de mi pc a mi consola, eso ya es el maximo merito de presumirte.",
+    x: 40,
+    y: 40,
+    imagen: "assets/primera.jpeg",
+    audio: ""
+  },
+  {
+    titulo: "Un mundo en cuadrado",
+    texto: "Aaaaaaaaaay como olvidar aquellos inicios de nuestra relación donde tan penosos los dos pero que poco a poco nos fuimos acercando más, unas ocaciones a distancia como lo que fue jugando y ahi me di cuenta que jugar contigo es muy cool por que nos la pasamos riendo, contigo dejo de ser una maquina que solo se conexta para destrozar, hasta la fecha haces que saque partes que no conocia de mi, muchas cosas que me han ayudado a mejorar como persona y otras que me estan ayudando a ser una mejor pareja para ti",
+    x: 80,
+    y: 20,
+    imagen: "assets/maincra.jpeg",
+    audio: ""
+  },
+   {
+    titulo: "Mi favorita parte 2",
+    texto: "Esta foto tambien es de mis favoritas, es la que menos presumo por que realmente esta foto es muuuuy nose, tiene un algo que hace que me guste demasiado demasiado y me encanta esta foto, todas las fotos que tengo tuyas estan en altar ya que son una joyita para mi, no se con que palabras expresarlo, pero bastara con que veas la forma en la que te miro cuando te tengo cerca, la forma en la que me pierdo en tu miradaestando a u lado, los nervios de verte a la cara siendo que ya han pasado 8 meses a tu lado, probablemente yo no te lo diga de forma muy seguida con palabras pero quiero que mis acciones te lo digan, que todo mi cuerpo te diga Me gustas! Te quiero! Te amo! Quiero que seas tu! Si a todo contigo menos al divorcio.",
+    x: 90,
+    y: 40,
+    imagen: "assets/envidioso.jpeg",
+    audio: ""
+  },
+  {
+    titulo: "Dos loquitos",
+    texto: "Me gusta mucho esos momentos donde nos sale la parte mas infantil, nuestra esencia misma, nuestro verdadero yo y nos lleva a disfrutar aun más nuestra compañia, momentos donde me siento seguro de ser quien soy contigo, sin filtros, sin mentiras, en ti encontre todo lo que no imagine buscar y es que contigo cada momento es unico, cada mentada de madre que me das, cada juzgada con la mirada que me lanzas, cada cosa por muy chica o grande la disfruto con unas ganas por que son cosas que estoy disfurtando contigo y eso no lo cambio por nada ni nadie.",
+    x: 10,
+    y: 20,
+    imagen: "assets/loquitos.jpeg",
+    audio: ""
+  },
+{
+    titulo: "Amarte",
+    texto: "Si me dieran la oportunidad de pedir un deseo, pediria mil vidas y en todas ellas me encargaría de buscarte, encontrarte y volver a enamorarnos. Po ruq esto que siento por ti es tan profundo e intenso que no cabe en esta vida, ni en un millon de ellas",
+    x: 30,
+    y: 90,
+    imagen: "assets/foto1.jpg",
+    audio: ""
+  },
+  {
+    titulo: "SaO",
+    texto: "Desde ahora mi vida te pertenece, soy tuyo, puedes hacer conmigo lo que quieras, solo dejame permanecer a tu lado.",
+    x: 20,
+    y: 70,
+    imagen: "",
+    audio: ""
+  },
+    {
+    titulo: "Amor",
+    texto: "Amor es una palabra pero tu eres el sentimiento de amar, te amo con mi alma, no te amo poco te amo con cada pedacito de mi corazón, te amare hasta que muera, hasta que no esté acá, no sé vivir sin ti, para mí eres todo,cuando te conoci conoci el significado de amor, solía tenía miedo a morir de amor pero cuando te conocí descubrí que había una manera de amarte inexplicable, hoy me gusta la vida mucho más que antes, te amo mi vida entera.",
+    x: 20,
+    y: 20,
+    imagen: "",
+    audio: ""
+  },
+  {
+    titulo: "Mi Noyolot",
+    texto: "Nimitstlasijtla ka no tonal pampa se tonali axkema miki",
+    x: 35,
+    y: 20,
+    imagen: "",
+    audio: ""
+  },
+    {
+    titulo: "La razón de por que te amo? No sé cuál es...",
+    texto: "Podria decir ques por tus ojazos, ojos tan lindos y brillantes que alumbran hasta mi mayor sombra, tal vez sea por tu risa, risa capaz de curar heridas, heridas que no sabia que tenia hasta que las sanaste, podria decir que tu voz, una voz tan linda, voz que se vuelve refugio en dias de tormento, tu forma de caminar?, por esos paso que das, pasos donde escribes versos en el suelo, pasos que dejan huella por la grandeza de tu alma, seran tus manos, manos que sin tocarme me sostienen, me acompañan y no me sueltan, podria ser que sea tu mirada, mirada que me entiende antes de entenderme a mi mismo, mirada que mira en lo más adentro de mi, pero la verdad es que es todo es una razón de amarte, ese algo invisible que hace que el mundo entere deje de importar cuando estas tu, cuando se trata de ti.",
+    x: 55,
+    y: 70,
+    imagen: "",
+    audio: ""
+  },
+  {
+    titulo: "Te dedico",
+    texto: "Te dedico todo mi tiempo, tiempo que estando contigo se vuelve el mejor tiempo, el tiempo que no estoy contigo pero que te mantengo en mi mente esperando por el tiempo que compartire contigo, te dedico todo mi amor, cada acto, cada palabra, cada pieza de mi corazpom, toma todo el amor de mi que es solo tuyo. te dedico todas mis noches de desvelo, noches por mas largas que sean siendo acompañado por ti, noches donde siempre termino pensanso en ti, te dedico mis pensamientos antes de ir a dormir y los pensamientos a la hora de despertar, pues desde el incicio de mi dia hasta el final del mismo estas presente, te dedico mis miedos , ya que contigo no le temo a nada, contigo logro calmar cada miedo mio, te dedico cada latido mio, cada aceleración estando a tu lado, cada latido diciendote te amo,te dedico mi paciencia, mi tiempo y mis intentos, cada suspiro, suspiros que salen del corazón cada que pienso en esa sonrisa tuyo, en esa calidez que sineto a tu lado, por como haces sentire a mi corazón, te dedico mi vida y lo mas valiovo de mi, mi alma, todo es para ti y por ti.",
+    x: 70,
+    y: 70,
+    imagen: "",
+    audio: ""
+  }
+];
+
+// Referencias
+const mapa = document.getElementById("mapa");
+const modal = document.getElementById("modal");
+const titulo = document.getElementById("tituloRecuerdo");
+const texto = document.getElementById("textoRecuerdo");
+const imagen = document.getElementById("imagenRecuerdo");
+const audio = document.getElementById("audioRecuerdo");
+const cerrar = document.getElementById("cerrar");
+
+// Crear estrellas
+recuerdos.forEach(recuerdo => {
+  const estrella = document.createElement("div");
+  estrella.classList.add("estrella");
+
+  estrella.style.left = recuerdo.x + "%";
+  estrella.style.top = recuerdo.y + "%";
+
+  estrella.addEventListener("click", () => {
+    titulo.textContent = recuerdo.titulo;
+    texto.textContent = recuerdo.texto;
+
+    if (recuerdo.imagen) {
+      imagen.src = recuerdo.imagen;
+      imagen.style.display = "block";
+    } else {
+      imagen.style.display = "none";
+    }
+
+    if (recuerdo.audio) {
+      audio.src = recuerdo.audio;
+      audio.style.display = "block";
+    } else {
+      audio.style.display = "none";
+    }
+
+    modal.classList.remove("hidden");
+  });
+
+  mapa.appendChild(estrella);
+});
+
+// Cerrar modal
+cerrar.addEventListener("click", () => {
+  modal.classList.add("hidden");
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.classList.add("hidden");
+  }
+});
